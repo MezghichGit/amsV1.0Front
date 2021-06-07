dba4700310e83732fefaa425bd27f6d57a551e44
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-06-07 12:31:18 +0100

    crud Provider

---
+++ src/app/pages/provider-add/provider-add.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ProviderService } from '../../services/provider.service';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-provider-add',
+  templateUrl: './provider-add.component.html',
+  styleUrls: ['./provider-add.component.css']
+})
+export class ProviderAddComponent implements OnInit {
+
+  provider: any;
+  constructor(private service: ProviderService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+  createProvider(myform) {
+    this.service.createProvider(myform).subscribe(
+      response => {
+        console.log(response);
+        this.router.navigate(['providerList']);
+      }
+    );
+  }
+
+}
